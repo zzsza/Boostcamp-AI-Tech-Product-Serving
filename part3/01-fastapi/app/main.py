0bf0186b338e1014b7a68c84f5e2035c9e31fc33
ahnsv <ahnsv@bc.edu>
2021-10-04 23:48:57 +0900

    feat(part3/01-fastapi): 주문 도메인 모델과 엔드포인트를 바인딩한다

--- part3/01-fastapi/app/main.py
+++ part3/01-fastapi/app/main.py
@@ -1,8 +1,10 @@
 import os
 import random
 from datetime import datetime
+from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
@@ -11,10 +13,21 @@ available_slots = int(os.getenv("AVAILABLE_SLOTS", 3))
 orders = []
 
 
+class Product(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    name: str
+    price: float
+
 class Order(BaseModel):
-    id: int
+    id: UUID = Field(default_factory=uuid4)
     status: str
+    products: List[Product] = Field(default_factory=list)
     created_at: datetime = Field(default_factory=datetime.now)
+
+    @property
+    def bill(self):
+        return sum([product.price for product in self.products])
+
 
 
 @app.get("/order", description="주문 리스트를 가져옵니다")
@@ -23,26 +36,34 @@ async def get_orders():
 
 
 @app.get("/order/{order_id}", description="주문 정보를 가져옵니다")
-async def get_order(order_id: int):
-    return next((order for order in orders if order.id == order_id))
+async def get_order(order_id: UUID):
+    try:
+        return next((order for order in orders if order.id == order_id))
+    except StopIteration:
+        return {"message": "주문 정보를 찾을 수 없습니다"}
 
 
 @app.post("/order", description="주문을 요청합니다", status_code=202)
-async def order():
+async def order(): # TODO(humphrey): 메뉴를 정의한다
     outstanding_orders = [order for order in orders if order.status == "IN_PROGRESS"]
     if len(outstanding_orders) >= available_slots:
         return {"message": "손님이 많습니다. 주문을 진행할 수 없습니다"}
 
-    new_order = Order(id=random.randint(1, 10000), status="STARTED", )
+    new_order = Order(status="STARTED", )
     orders.append(new_order)
     return new_order.id
 
 
 @app.patch("/order", description="주문을 수정합니다")
 async def update_order():
-    pass
+    pass # TODO(humphrey): 주문 상태를 수정한다
 
 
 @app.get("/bill/{order_id}", description="계산을 요청합니다")
-async def get_bill(order_id: int):
-    pass
+async def get_bill(order_id: UUID):
+    try:
+        found_order = next((order for order in orders if order.id == order_id))
+        return found_order.bill
+    except StopIteration:
+        return {"message": "주문 정보를 찾을 수 없습니다"}
+
