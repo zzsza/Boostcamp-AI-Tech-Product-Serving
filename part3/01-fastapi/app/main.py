86c9d8a90d5b7f4ab7521bc93ee7dad3690b4e9e
ahnsv <ahnsv@bc.edu>
2021-10-20 21:57:19 +0900

    refactor(01-fastapi): type hint를 추가하고, 코드를 간결하게한다

--- part3/01-fastapi/app/main.py
+++ part3/01-fastapi/app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 from datetime import datetime
-from typing import Any, List
+from typing import List, Optional, Union
 from fastapi import FastAPI, File, UploadFile
 from fastapi.param_functions import Depends
 from pydantic import BaseModel, Field
@@ -20,26 +20,33 @@ app = FastAPI()
 
 available_slots = int(os.getenv("AVAILABLE_SLOTS", 3))
 
-orders = []
+orders = []  # TODO(humphrey): repository 객체를 생성한다
+
+
+def can_order() -> bool:
+    outstanding_orders = [order for order in orders if order.status == "IN_PROGRESS"]
+    return len(outstanding_orders) < available_slots
+
 
 def transform_image(image_bytes: bytes):
-    transform = albumentations.Compose([
+    transform = albumentations.Compose(
+        [
             albumentations.Resize(height=512, width=384),
-            albumentations.Normalize(mean=(0.5, 0.5, 0.5), 
-                                     std=(0.2, 0.2, 0.2)),
-            albumentations.pytorch.transforms.ToTensorV2()
-        ])
+            albumentations.Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
+            albumentations.pytorch.transforms.ToTensorV2(),
+        ]
+    )
     image = Image.open(io.BytesIO(image_bytes))
-    image = image.convert('RGB')
+    image = image.convert("RGB")
     image_array = np.array(image)
-    return transform(image=image_array)['image'].unsqueeze(0)
+    return transform(image=image_array)["image"].unsqueeze(0)
 
-def predict_from_image_byte(image_bytes: bytes, model) -> List[int]:
+
+def predict_from_image_byte(image_bytes: bytes, model: MyEfficientNet) -> List[int]:
     transformed_image = transform_image(image_bytes)
     outputs = model.forward(transformed_image)
     _, y_hat = outputs.max(1)
     return y_hat.tolist()
-
 
 
 class Product(BaseModel):
@@ -47,10 +54,11 @@ class Product(BaseModel):
     name: str
     price: float
 
+
 class InferenceImageProduct(Product):
     name: str = "inference_image_product"
     price: float = 100.0
-    result: Any # FIXME(humphrey): tensor type일 때 json serialize가 안된다
+    result: Optional[List]
 
 
 class Order(BaseModel):
@@ -64,14 +72,13 @@ class Order(BaseModel):
         return sum([product.price for product in self.products])
 
 
-
 @app.get("/order", description="주문 리스트를 가져옵니다")
-async def get_orders():
+async def get_orders() -> List[Order]:
     return orders
 
 
 @app.get("/order/{order_id}", description="주문 정보를 가져옵니다")
-async def get_order(order_id: UUID):
+async def get_order(order_id: UUID) -> Union[Order, dict]:
     try:
         return next((order for order in orders if order.id == order_id))
     except StopIteration:
@@ -79,27 +86,32 @@ async def get_order(order_id: UUID):
 
 
 @app.post("/order", description="주문을 요청합니다", status_code=202)
-async def order(file: UploadFile = File(...), model: MyEfficientNet = Depends(get_model)): 
-    outstanding_orders = [order for order in orders if order.status == "IN_PROGRESS"]
-    if len(outstanding_orders) >= available_slots:
+async def order(
+    file: UploadFile = File(...), model: MyEfficientNet = Depends(get_model)
+) -> Union[Order, dict]:  # TODO(humphrey): multiple file upload를 가능하게 한다
+    if not can_order():
         return {"message": "손님이 많습니다. 주문을 진행할 수 없습니다"}
-    
-    product = InferenceImageProduct(result=None) 
-    new_order = Order(status="IN_PROGRESS", products=[product]) # TODO(humphrey): queue에 inference task를 넣고, background에 돌려야하나?
+
+    product = InferenceImageProduct(result=None)
+    new_order = Order(status="IN_PROGRESS", products=[product])
     orders.append(new_order)
 
     image_bytes = await file.read()
+    # TODO(humphrey): background에서 prediction과 domain model update를 수행한다
     inference_result = predict_from_image_byte(image_bytes=image_bytes, model=model)
+    # TODO(humphrey): update order 함수를 활용해서 코드를 간소화한다
     existing_order = await get_order(new_order.id)
+    if isinstance(existing_order, dict):
+        return {"message": "주문 정보를 찾을 수 없습니다"}
     existing_order.status = "DONE"
-    existing_order.products[0].result=inference_result
+    existing_order.products[0].result = inference_result
 
     return existing_order
 
 
 @app.patch("/order", description="주문을 수정합니다")
 async def update_order():
-    pass # TODO(humphrey): 주문 상태를 수정한다
+    pass  # TODO(humphrey): 주문 상태를 수정한다
 
 
 @app.get("/bill/{order_id}", description="계산을 요청합니다")
@@ -109,4 +121,3 @@ async def get_bill(order_id: UUID):
         return found_order.bill
     except StopIteration:
         return {"message": "주문 정보를 찾을 수 없습니다"}
-
