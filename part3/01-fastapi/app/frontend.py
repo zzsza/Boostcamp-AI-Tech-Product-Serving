2ecc81c4f995ead38b54c4a9beebda0242c4344b
humphrey <ahnsv@bc.edu>
2021-11-16 23:26:07 +0900

    feat(part3/01-fastapi): fastapi와 streamlit을 연결한다 (#29)
    * feat(part3/01-fastapi): streamlit frontend를 연결한다
    
    * refactor(part3/01-fastapi): streamlit과 웹서버를 연결하는 make command를 만든다

---
+++ part3/01-fastapi/app/frontend.py
@@ -0,0 +1,52 @@
+import io
+import os
+from pathlib import Path
+
+import requests
+import yaml
+from PIL import Image
+
+import streamlit as st
+from app.confirm_button_hack import cache_on_button_press
+
+# SETTING PAGE CONFIG TO WIDE MODE
+ASSETS_DIR_PATH = os.path.join(Path(__file__).parent.parent.parent.parent, "assets")
+
+st.set_page_config(layout="wide")
+
+root_password = 'password'
+
+
+def main():
+    st.title("Mask Classification Model")
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file:
+        image_bytes = uploaded_file.getvalue()
+        image = Image.open(io.BytesIO(image_bytes))
+
+        st.image(image, caption='Uploaded Image')
+        st.write("Classifying...")
+
+        files = [
+            ('files', (uploaded_file.name, image_bytes,
+                       uploaded_file.type))
+        ]
+        response = requests.post("http://localhost:8001/order", files=files)
+        label = response.json()["products"][0]["result"]
+        st.write(f'label is {label}')
+
+
+@cache_on_button_press('Authenticate')
+def authenticate(password) -> bool:
+    print(type(password))
+    return password == root_password
+
+
+password = st.text_input('password', type="password")
+
+if authenticate(password):
+    st.success('You are authenticated!')
+    main()
+else:
+    st.error('The password is invalid.')
