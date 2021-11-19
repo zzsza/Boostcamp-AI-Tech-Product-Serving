b41a2c2da7370b34d63fe296ef75008a4814dbac
Sung Yun Byeon <zzsza@naver.com>
2021-11-19 15:04:43 +0900

    refactor: 코드 설명을 추가한다 (#34)
    * feat(part4/01-bentoml): 예제를 추가한다
    
    * refactor: 코드 설명을 추가한다

--- part4/01-bentoml/examples/bento_packer.py
+++ part4/01-bentoml/examples/bento_packer.py
@@ -1,13 +1,21 @@
 # bento_packer.py
+# 모델 학습
+from sklearn import svm
+from sklearn import datasets
 
-# import the IrisClassifier class defined above
+clf = svm.SVC(gamma='scale')
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+clf.fit(X, y)
+
+# bento_service.py에서 정의한 IrisClassifier
 from bento_service import IrisClassifier
 
-# Create a iris classifier service instance
+# IrisClassifier 인스턴스 생성
 iris_classifier_service = IrisClassifier()
 
-# Pack the newly trained model artifact
+# Model Artifact를 Pack
 iris_classifier_service.pack('model', clf)
 
-# Save the prediction service to disk for model serving
+# Model Serving을 위한 서비스를 Disk에 저장
 saved_path = iris_classifier_service.save()
