03bffecbe171d29713e3861f225e5ef515ef6bbc
ahnsv <ahnsv@bc.edu>
2021-11-21 10:03:38 +0900

    style(part3/03-logging): black formatter를 적용한다

--- part3/03-logging/examples/logging_module.py
+++ part3/03-logging/examples/logging_module.py
@@ -8,29 +8,21 @@ logger.info("hello world")  # 아무런 로그도 출력되지 않습니다.
 import logging.config
 
 logger_config = {
-    'version': 1,  # required
-    'disable_existing_loggers': True,  # 다른 Logger를 overriding 합니다
-    'formatters': {
-        'simple': {
-            'format': '%(asctime)s | %(levelname)s - %(message)s'
-        },
+    "version": 1,  # required
+    "disable_existing_loggers": True,  # 다른 Logger를 overriding 합니다
+    "formatters": {
+        "simple": {"format": "%(asctime)s | %(levelname)s - %(message)s"},
     },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         }
     },
-    'loggers': {
-        'example': {
-            'level': 'INFO',
-            'handlers': ['console']
-        }
-    }
+    "loggers": {"example": {"level": "INFO", "handlers": ["console"]}},
 }
 
 logging.config.dictConfig(logger_config)
 logger_with_config = logging.getLogger("example")
 logger_with_config.info("이제는 보이죠?")
-
