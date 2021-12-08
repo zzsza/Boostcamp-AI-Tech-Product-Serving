6af4f665fbfcd1ea75db3bc9d5f05cabbe8a052a
abbymark <abbymark@naver.com>
2021-12-08 12:20:58 +0000

    cicd

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Docker Build Deploy")
+    st.title("Mask Classification Model - Docker Build Deploy- CICD하려고 바꿈")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
