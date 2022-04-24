1687b2d21220676b163626651de97fa032e873b7
Sung Yun Byeon <zzsza@naver.com>
2022-04-24 22:19:38 +0900

    refactor/part4-airflow:Airflow Kwargs 설명 수정
    refactor/part4-airflow:Airflow Keyword Argument 설명을 수정한다

--- part4/02-airflow/dags/03-python-operator-with-context.py
+++ part4/02-airflow/dags/03-python-operator-with-context.py
@@ -19,21 +19,20 @@ def print_current_date_provide_context(*args, **kwargs):
     """
     provide_context=True로 지정하면 kwargs 다양한 값들이 저장됨
     {'dag': <DAG: python_dag_with_jinja>,
-    'ds': '2020-02-10',
-    'next_ds': '2020-02-11',
-    'next_ds_nodash': '20200211',
-    'prev_ds': '2020-02-09',
-    'prev_ds_nodash': '20200209',
-    'ds_nodash': '20200210',
-    'ts': '2020-02-10T00:30:00+00:00',
-    'ts_nodash': '20200210T003000',
-    'ts_nodash_with_tz': '20200210T003000+0000',
-    'yesterday_ds': '2020-02-09',
-    'yesterday_ds_nodash': '20200209',
-    'tomorrow_ds': '2020-02-11',
-    'tomorrow_ds_nodash': '20200211',
-    'end_date': '2020-02-10',
-    'execution_date': <Pendulum [2020-02-10T00:30:00+00:00]> ...}
+    'ds': '2022-04-20',
+    'next_ds': '2022-04-21',
+    'next_ds_nodash': '20220421',
+    'prev_ds': '2022-04-19',
+    'prev_ds_nodash': '20220419',
+    'ds_nodash': '20220420',
+    'ts': '2022-04-20T00:30:00+00:00',
+    'ts_nodash': '20220420T003000',
+    'ts_nodash_with_tz': '20220420T003000+0000',
+    'yesterday_ds': '2022-04-19',
+    'yesterday_ds_nodash': '20240419',
+    'tomorrow_ds': '2022-04-21',
+    'tomorrow_ds_nodash': '20220421',
+    'execution_date': <Pendulum [2022-04-20T00:30:00+00:00]> ...}
     """
     print(f"kwargs :{kwargs}")
     execution_date = kwargs['ds']
