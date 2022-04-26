934dd217754da8d00d4b22057f574007aa087a3e
Sung Yun Byeon <zzsza@naver.com>
2022-04-27 01:06:30 +0900

    refactor/part4-airflow: Airflow Example 코드 정리 & 설명 추가 (#86)

--- part4/02-airflow/dags/03-python-operator-with-context.py
+++ part4/02-airflow/dags/03-python-operator-with-context.py
@@ -2,19 +2,24 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
+# 앞선 02-python-operator.py는 "date_now = datetime.now().date()"를 사용하기 때문에
+# 언제 실행해도 실행하는 시간 기준으로 실행됨
+# Airflow는 Batch 성으로 특정 시간대로 실행하는 도구인데, 위와 같이 now 등을 사용하면 실행하는 시점이 아닌 현재 시점으로 실행됨
+# Airflow에선 항상 현재 최신 작업만 실행하는 것이 아니고, 과거 날짜로 실행해야 하는 경우도 존재(Backfill이란 용어 사용)
+# 따라서 코드 상에서 now(), current_datetime() 등을 사용하지 않고, Airflow에서 실행하기로 했던 시간을 넣어줘야 함
+# 이를 execution_date라고 부름
+
+
 default_args = {
-    'owner': 'kyle',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 4, 20),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-
-dag = DAG('python_dag_with_context',
-          default_args=default_args,
-          schedule_interval='30 0 * * *')
+                   'owner': 'kyle',
+                   'depends_on_past': False,
+                   'start_date': datetime(2022, 4, 20),
+                   'retries': 1,
+                   'retry_delay': timedelta(minutes=5),
+               },
 
 
+# 사용할 함수 정의
 def print_current_date_provide_context(*args, **kwargs):
     """
     provide_context=True로 지정하면 kwargs 다양한 값들이 저장됨
@@ -42,13 +47,17 @@ def print_current_date_provide_context(*args, **kwargs):
     print(f"{execution_date}는 {date_kor[datetime_weeknum]}요일입니다")
 
 
+# with 구문으로 DAG 정의
+with DAG(
+        dag_id='python_dag_with_context',
+        default_args=default_args,
+        schedule_interval='30 0 * * *',
+        tags=['my_dags'],
+) as dag:
+    python_task_context = PythonOperator(
+        task_id='print_current_date_with_context_variable',
+        python_callable=print_current_date_provide_context,
+        provide_context=True  # True일 경우 Task Instance의 Attribute를 Keyword Argument로 받아서 사용할 수 있음
+    )
 
-python_task_context = PythonOperator(
-    task_id='print_current_date_with_context_variable',
-    python_callable=print_current_date_provide_context,
-    provide_context=True,
-    dag=dag,
-)
-
-
-python_task_context
+    python_task_context
--- part4/02-airflow/dags/04-python-operator-with-jinja.py
+++ part4/02-airflow/dags/04-python-operator-with-jinja.py
@@ -1,43 +1,43 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
 from datetime import datetime, timedelta
 
+# 앞의 03 예제는 provide_context=True 옵션을 주고 Attribute에 접근함
+# 이번엔 이 방식이 아닌 Airflow의 Template 방식을 사용
+# Python에선 큰 차이를 못 느낄 수 있지만, SQL Operator 등에선 유용하게 사용됨(쿼리문에 Airflow Template을 사용)
+# Jinja Template : Airflow에 미리 정의된 템플릿으로 {{ ds }}, {{ ds_nodash }} 라고 정의해서 Airflow Operator에 넘겨주면 실행하는 과정에서 템플릿 기반으로 값이 변환됨
+
 default_args = {
-    'owner': 'kyle',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 4, 20),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-
-dag = DAG('python_dag_with_jinja',
-          default_args=default_args,
-          schedule_interval='30 0 * * *')
+                   'owner': 'kyle',
+                   'depends_on_past': False,
+                   'start_date': datetime(2022, 4, 20),
+                   'retries': 1,
+                   'retry_delay': timedelta(minutes=5),
+               },
 
 
-def print_current_date_jinja(*args, **kwargs):
-    """
-    jinja template(today)가 templates_dict으로 저장되서 kwargs에서 사용할 수 있음
-    """
-    execution_date = kwargs.get('templates_dict').get('today', None)
+# 사용할 함수 정의
+def print_current_date_jinja(execution_date):
     execution_date = datetime.strptime(execution_date, "%Y-%m-%d").date()
     date_kor = ["월", "화", "수", "목", "금", "토", "일"]
     datetime_weeknum = execution_date.weekday()
     print(f"{execution_date}는 {date_kor[datetime_weeknum]}요일입니다")
 
 
-today = "{{ ds }}"
+# with 구문으로 DAG 정의
+with DAG(
+        dag_id='python_dag_with_jinja',
+        default_args=default_args,
+        schedule_interval='30 0 * * *',
+        tags=['my_dags'],
+) as dag:
+    execution_date = "{{ ds }}"  # Template 정의
 
-python_task_jinja = PythonOperator(
-    task_id='print_current_date_with_jinja',
-    python_callable=print_current_date_jinja,
-    provide_context=True,
-    templates_dict={
-        'today': today,
-    },
-    dag=dag,
-)
+    python_task_jinja = PythonOperator(
+        task_id='print_current_date_with_jinja',
+        python_callable=print_current_date_jinja,
+        op_args=[execution_date],  # Argument 주입
+        dag=dag,
+    )
 
-
-python_task_jinja
+    python_task_jinja
