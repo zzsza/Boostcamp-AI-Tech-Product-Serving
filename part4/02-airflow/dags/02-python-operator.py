8e1784eee134a34574a157c9f0e22e1188e11512
Sung Yun Byeon <zzsza@naver.com>
2022-04-27 01:06:03 +0900

    refactor/part4-airflow: Airflow Example 코드 정리 & 설명 추가

--- part4/02-airflow/dags/02-python-operator.py
+++ part4/02-airflow/dags/02-python-operator.py
@@ -3,18 +3,15 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'kyle',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 4, 20),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
-}
-
-dag = DAG('python_dag1',
-          default_args=default_args,
-          schedule_interval='30 0 * * *')
+                   'owner': 'kyle',
+                   'depends_on_past': False,
+                   'start_date': datetime(2022, 4, 20),
+                   'retries': 1,
+                   'retry_delay': timedelta(minutes=5),
+               },
 
 
+# 사용할 함수 정의
 def print_current_date():
     date_kor = ["월", "화", "수", "목", "금", "토", "일"]
     date_now = datetime.now().date()
@@ -22,10 +19,17 @@ def print_current_date():
     print(f"{date_now}는 {date_kor[datetime_weeknum]}요일입니다")
 
 
-python_task = PythonOperator(
-    task_id='print_current_date',
-    python_callable=print_current_date,
-    dag=dag,
-)
+# with 구문으로 DAG 정의
+with DAG(
+        dag_id='python_dag1',
+        default_args=default_args,
+        schedule_interval='30 0 * * *',
+        tags=['my_dags'],
+) as dag:
+    python_task = PythonOperator(
+        task_id='print_current_date',
+        python_callable=print_current_date # 실행할 파이썬 함수
+    )
 
-python_task
+    python_task
+
