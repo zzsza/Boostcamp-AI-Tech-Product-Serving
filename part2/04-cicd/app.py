fb51d91f98148eec14f53499b637b879e51dcd37
DongSeok Lee/이동석 <41297473+dongseoklee1541@users.noreply.github.com>
2022-05-30 12:26:24 +0900

    Update app.py

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - github Build Deploy")
+    st.title("Mask Classification Model - Github Build Deploy")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
