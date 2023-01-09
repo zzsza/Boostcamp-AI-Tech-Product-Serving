68b49d45c0dfa42fb87a1002dce73b6e88913302
Hyejin Shin <jstep0000@gmail.com>
2023-01-10 02:10:53 +0900

    Update app.py

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Docker Build Deploy")
+    st.title("Mask Classification Model - Docker ACTION Deploy")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
