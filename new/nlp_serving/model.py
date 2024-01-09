023e7e287aa9764cf3a0daf13bf98526a82e0438
ahnsv <ahnsv@bc.edu>
2024-01-08 23:17:46 -0800

    feat: load

--- new/nlp_serving/model.py
+++ new/nlp_serving/model.py
@@ -1,6 +1,10 @@
 import argparse
+import os
 
 import pandas as pd
+
+from tqdm import tqdm
+import requests
 
 from tqdm.auto import tqdm
 
@@ -8,6 +12,8 @@ import transformers
 import torch
 import torchmetrics
 import pytorch_lightning as pl
+
+NLP_PT_FILE_ID = "1UYrKf4fMneL_stswb1rRzeQEbHcqXV-D"
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -165,39 +171,77 @@ class Model(pl.LightningModule):
         return optimizer
 
 
+URL = "https://docs.google.com/uc?export=download&confirm=1"
+CHUNK_SIZE = 32768
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    with open(destination, "wb") as f:
+        for chunk in tqdm(response.iter_content(CHUNK_SIZE)):
+            if chunk:
+                f.write(chunk)
+
+
+def download_from_google_drive(file_id: str, destination_path: str):
+    if os.path.exists(destination_path):
+        print("File already exists.")
+        return
+    response = requests.get(URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = requests.get(URL, params=params)
+
+    save_response_content(response, destination_path)
+
+
 if __name__ == '__main__':
     # 하이퍼 파라미터 등 각종 설정값을 입력받습니다
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='klue/roberta-small', type=str)
+    parser.add_argument('--model_path', default='data/model.pt', type=str)
     parser.add_argument('--batch_size', default=16, type=int)
     parser.add_argument('--max_epoch', default=1, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
-    parser.add_argument('--train_path', default='../data/train.csv')
-    parser.add_argument('--dev_path', default='../data/dev.csv')
-    parser.add_argument('--test_path', default='../data/dev.csv')
-    parser.add_argument('--predict_path', default='../data/test.csv')
-    args = parser.parse_args(args=[])
+    parser.add_argument('--train_path', default='data/train.csv')
+    parser.add_argument('--dev_path', default='data/dev.csv')
+    parser.add_argument('--test_path', default='data/dev.csv')
+    parser.add_argument('--predict_path', default='data/test.csv')
+    args = parser.parse_args()
+
+    download_from_google_drive(file_id=NLP_PT_FILE_ID, destination_path=args.model_path)
 
     # dataloader와 model을 생성합니다.
     dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
                             args.test_path, args.predict_path)
 
     # gpu가 없으면 'gpus=0'을, gpu가 여러개면 'gpus=4'처럼 사용하실 gpu의 개수를 입력해주세요
-    trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=args.max_epoch, log_every_n_steps=1)
+    if torch.cuda.is_available():
+        trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=args.max_epoch, log_every_n_steps=1)
+    else:
+        trainer = pl.Trainer(accelerator="cpu", max_epochs=args.max_epoch, log_every_n_steps=1)
 
     # Inference part
     # 저장된 모델로 예측을 진행합니다.
-    model = torch.load('model.pt')
+    model = torch.load(args.model_path)
     predictions = trainer.predict(model=model, datamodule=dataloader)
 
     # 예측된 결과를 형식에 맞게 반올림하여 준비합니다.
     predictions = list(round(float(i), 1) for i in torch.cat(predictions))
 
     # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
-    output = pd.read_csv('../data/sample_submission.csv')
+    output = pd.read_csv('./data/sample_submission.csv')
     output['target'] = predictions
     output.to_csv('output.csv', index=False)
-
