42d42375dcb2c988b7023eb327bfce5441d9ac35
noeunkim <noeun.kim@wesang.com>
2024-01-11 05:31:16 +0900

    Minor changes

--- new/recsys_serving/models/My_model.py
+++ new/recsys_serving/models/My_model.py
@@ -3,18 +3,14 @@ import torch.nn as nn
 
 
 class MyModel(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self):
         super().__init__()
-
         """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
+        모델 아키텍쳐를 직접 작성합니다
         """
 
     def forward(self, x):
         """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
+        위 모델에 대한 forward propagation
         """
         return x
