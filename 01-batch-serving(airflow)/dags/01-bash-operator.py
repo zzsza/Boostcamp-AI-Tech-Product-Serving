6de7e8bd3e97880ac6f7b7866af70721b2581072
zzsza <zzsza@naver.com>
2024-01-29 11:57:08 +0900

    feat: Airflow on_failure_callback 적용

--- 01-batch-serving(airflow)/dags/01-bash-operator.py
+++ 01-batch-serving(airflow)/dags/01-bash-operator.py
@@ -10,9 +10,6 @@ default_args = {
     'retry_delay': timedelta(minutes=5)  # 만약 실패하면 5분 뒤 재실행
     # 'priority_weight': 10 # DAG의 우선 순위를 설정할 수 있음
     # 'execution_timeout': timedelta(seconds=300), # 실행 타임아웃 : 300초 넘게 실행되면 종료
-    # 'on_failure_callback': some_function # 만약에 Task들이 실패하면 실행할 함수
-    # 'on_success_callback': some_other_function
-    # 'on_retry_callback': another_function
 }
 
 # with 구문으로 DAG 정의
