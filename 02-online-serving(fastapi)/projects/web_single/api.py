02d4e704ab8eee4c607e674303c91b15f50935cf
zzsza <zzsza@naver.com>
2024-02-09 15:34:22 +0900

    feat: FastAPI Code with Comment

--- 02-online-serving(fastapi)/projects/web_single/api.py
+++ 02-online-serving(fastapi)/projects/web_single/api.py
@@ -1,43 +1,44 @@
 from fastapi import APIRouter, HTTPException, status
-from pydantic import BaseModel
-from sqlmodel import Session
-
-from database import PredictionResult, engine
+from schemas import PredictionRequest, PredictionResponse
 from dependencies import get_model
+from database import PredictionResult, engine
+from sqlmodel import Session, select
 
 router = APIRouter()
 
-
-class PredictionRequest(BaseModel):
-    features: list
-
-
-class PredictionResponse(BaseModel):
-    id: int
-    result: int
-
-
-# FastAPI 경로
 @router.post("/predict")
 def predict(request: PredictionRequest) -> PredictionResponse:
-    # 모델 추론
-    model = get_model()
+    # 모델 load
+    model = get_model()    
+    
+    # 예측 : 여러분들의 코드 상황에 따라 구현
     prediction = int(model.predict([request.features])[0])
-
-    # 결과를 데이터베이스에 저장
+     
+    # 예측한 결과를 DB에 저장
+    # 데이터베이스 객체를 생성. 그 때 prediction을 사용
     prediction_result = PredictionResult(result=prediction)
     with Session(engine) as session:
         session.add(prediction_result)
         session.commit()
         session.refresh(prediction_result)
 
-    # 응답
     return PredictionResponse(id=prediction_result.id, result=prediction)
 
+    # return PredictionResponse
+
+@router.get("/predict")
+def get_predictions() -> list[PredictionResponse]:
+    with Session(engine) as session:
+        statement = select(PredictionResult)
+        prediction_results = session.exec(statement).all()
+        # prediction_results = session.query(PredictionResult).all()
+        return [
+            PredictionResponse(id=prediction_result.id, result=prediction_result.result)
+            for prediction_result in prediction_results
+        ]
 
 @router.get("/predict/{id}")
-def get_prediction(id: int) -> PredictionResponse:
-    # 데이터베이스에서 결과를 가져옴
+def get_preidction(id: int) -> PredictionResponse:
     with Session(engine) as session:
         prediction_result = session.get(PredictionResult, id)
         if not prediction_result:
@@ -48,13 +49,3 @@ def get_prediction(id: int) -> PredictionResponse:
             id=prediction_result.id, result=prediction_result.result
         )
 
-
-@router.get("/predict")
-def get_predictions() -> list[PredictionResponse]:
-    # 데이터베이스에서 결과를 가져옴
-    with Session(engine) as session:
-        prediction_results = session.query(PredictionResult).all()
-        return [
-            PredictionResponse(id=prediction_result.id, result=prediction_result.result)
-            for prediction_result in prediction_results
-        ]
